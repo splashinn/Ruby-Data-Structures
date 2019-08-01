c42ce2b2b747a03f0ebcf2b34c20a3ecf20d0aa2
Kyle Maune <the.kyle.maune@gmail.com>
2019-08-01 09:52:01 -0700

    add fizzbuzz example in elixir

---
+++ elixir/fizzbuzz.ex
@@ -0,0 +1,11 @@
+whichfizz = fn
+  (0, 0, _) -> "FizzBuzz"
+  (0, _, _) -> "Fizz"
+  (_, _, n) -> n
+end
+
+fizzbuzz = fn (n) -> 
+  whichfizz.(rem(n, 3), rem(n, 5), n)
+end
+
+IO.inspect Enum.map(1..100, fizzbuzz)
